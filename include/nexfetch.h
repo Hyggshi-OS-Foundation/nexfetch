00cc0a1f80a1a62f6cf85c866e1ab5d471edfb79
HyggshiOSDeveloper <hyggshidev@gmail.com>
2026-08-14 09:51:14 +0700

    fix p6

--- include/nexfetch.h
+++ include/nexfetch.h
@@ -5,7 +5,7 @@
 #include "platform.h"
 #include "util.h"
 
-#define NEXFETCH_VERSION "1.1.4"
+#define NEXFETCH_VERSION "1.1.5"
 #define MAX_MODULES 48
 #define MAX_PLUGINS 16
 #define MAX_VAL_LEN 512
